1ae472c1a52b9b726c3bd678345ff81d18f4ff1b
James N. V. Cash <james.nvc@gmail.com>
2020-06-23 11:25:19 -0400

    Example solution for birthday problem

--- src/katas/birthday/core.clj
+++ src/katas/birthday/core.clj
@@ -9,29 +9,67 @@
    (java.time.format DateTimeFormatter)
    (java.time.temporal ChronoUnit)))
 
+(defn ->key
+  [header]
+  (keyword (string/replace header #"_" "-")))
+
+(defn load-csv
+  "Load the csv from the resource file, converting it to a sequence of maps.
+  Assumes the first row is a header row that gives the name of each column."
+  [file-name]
+  (with-open [f (io/reader (io/resource file-name))]
+    (let [[header & rows] (csv/read-csv f)
+          header (map ->key header)]
+      (doall (map (partial zipmap header) rows)))))
+
+(defn parse-birthdate
+  "Parse a birthdate string in the form yyyy/MM/dd into a local date"
+  [date-str]
+  (LocalDate/parse date-str (DateTimeFormatter/ofPattern "yyyy/MM/dd")))
+
+(defn birthday-today?
+  "Given a map representing one user, return true if their birthday is
+  today, where today is given as an argument"
+  [^LocalDate today date-of-birth]
+  (string/ends-with?
+    date-of-birth
+    (.format today (DateTimeFormatter/ofPattern "MM/dd"))))
+
+(defn age
+  [date-of-birth today]
+  (.between ChronoUnit/YEARS date-of-birth today))
+
+(defn make-email
+  [{:keys [name email date-of-birth]}]
+  {:from "me@example.com"
+   :to email
+   :subject "Happy Birthday!"
+   :body (str "Happy Birthday " name "! "
+              "Wow, you're "
+              (age (parse-birthdate date-of-birth)
+                   (LocalDate/now))
+              " already!")})
+
+(comment
+  (load-csv "birthday/employees.csv")
+  )
+
+(defn emails-for-day
+  [day]
+  (->> (load-csv "birthday/employees.csv")
+       (filter (partial birthday-today? day))
+       (map make-email)))
+
+(defn send-email!
+  [msg]
+  (postal/send-message
+    ;; [TODO] load config from somewhere
+    {:host "localhost"
+     :user "azurediamond"
+     :pass "hunter2"
+     :port 2525}
+    msg))
+
 (defn greet! []
-  (->> (io/resource "birthday/employees.csv")
-       (io/reader)
-       (csv/read-csv)
-       rest
-       (filter (fn [row]
-                 (string/ends-with?
-                   (row 2)
-                   (.format (LocalDate/now) (DateTimeFormatter/ofPattern "MM/dd")))))
-       (map (fn [row]
-                 (postal/send-message
-                  {:host "localhost"
-                   :user "azurediamond"
-                   :pass "hunter2"
-                   :port 2525}
-                  {:from "me@example.com"
-                   :to (row 1)
-                   :subject "Happy Birthday!"
-                   :body (str "Happy Birthday " (row 0) "! "
-                              "Wow, you're "
-                              (.between ChronoUnit/YEARS
-                                        (LocalDate/parse (row 2)
-                                                         (DateTimeFormatter/ofPattern "yyyy/MM/dd"))
-                                        (LocalDate/now))
-                              " already!")})))
-       doall))
+  (doseq [email (emails-for-day (LocalDate/now))]
+    (send-email! email)))
--- test/katas/test/birthday.clj
+++ test/katas/test/birthday.clj
@@ -1,9 +1,37 @@
 (ns katas.test.birthday
   (:require
     [clojure.test :refer :all]
-    [katas.birthday.core :as birthday]))
+    [katas.birthday.core :as birthday])
+  (:import
+   (java.time LocalDate)))
 
-(deftest greet!
-  (testing "sends email to everyone with birthday today"
-    ;; How do we test???
-    #_(greet!)))
+(deftest ->key
+  (testing "turn strings into keys for the map"
+    (is (= :foo (birthday/->key "foo")))
+    (is (= :foo-bar (birthday/->key "foo_bar")))
+    (is (= :foo-bar (birthday/->key "foo-bar")))
+    (is (= :foo-bar-baz (birthday/->key "foo_bar_baz")))
+    (is (= :foo-bar-baz (birthday/->key "foo-bar_baz")))))
+
+(deftest birthday-today?
+  (testing "checks if birthday is today"
+    (is (true? (birthday/birthday-today?
+                 (LocalDate/of 1848 3 6)
+                 "1383/03/06")))
+    (is (true? (birthday/birthday-today?
+                 (LocalDate/of 1848 3 6)
+                 "1970/03/06")))
+    (is (false?
+          (birthday/birthday-today?
+            (LocalDate/of 1848 3 6)
+            "1383/06/03")))))
+
+(deftest age
+  (testing "Get the age in years"
+    (is (= 30 (birthday/age (LocalDate/of 1989 7 22)
+                            (LocalDate/of 2020 6 23))))
+    (is (= -30 (birthday/age (LocalDate/of 2020 6 23)
+                             (LocalDate/of 1989 7 22))))
+
+    (is (= 100 (birthday/age (LocalDate/of 1920 6 23)
+                             (LocalDate/of 2020 6 23) )))))
