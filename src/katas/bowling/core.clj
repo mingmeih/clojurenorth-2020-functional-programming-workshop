ab48d2d3507a245584d11533f83db5e26e862421
James N. V. Cash <james.nvc@gmail.com>
2020-06-23 18:29:36 -0400

    Working, albiet not very functional

--- src/katas/bowling/core.clj
+++ src/katas/bowling/core.clj
@@ -1,4 +1,32 @@
 (ns katas.bowling.core)
 
 (defn total-score
-  [rolls])
+  [rolls]
+  (loop [rolls rolls
+         frame 1
+         score 0]
+    (if (empty? rolls)
+      score
+      (let [[r1 r2 & next] rolls]
+        (cond
+          (= 10 r1)
+          (if (= 10 frame)
+            (+ score r1 r2 (first next))
+            (recur
+              (cons r2 next)
+              (inc frame)
+              (+ score r1 r2 (first next))))
+
+          (= 10 (+ r1 r2))
+          (if (= 10 frame)
+            (+ score 10 (first next))
+            (recur
+              next
+              (inc frame)
+              (+ score 10 (first next))))
+
+          :else
+          (recur
+            next
+            (inc frame)
+            (+ score r1 r2)))))))
