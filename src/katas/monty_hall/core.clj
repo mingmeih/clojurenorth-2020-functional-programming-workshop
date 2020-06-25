d2b076e42f4da6af05f847093ee9ddd295c95ab7
Mingmei Huang <mingmeihuang1@gmail.com>
2020-06-25 12:06:06 -0400

    : O

--- src/katas/monty_hall/core.clj
+++ src/katas/monty_hall/core.clj
@@ -6,11 +6,11 @@
 ;; each key is a door, and val of 0 is a goat, val of 1 is a car
 
 (defn simulate [n]
-  (reduce (fn [result _]
-            (let [choice (rand-int 3)
-                  switch (val (last (dissoc doors choice)))]
-              ;; out of the 2 remaining doors, the first will be a goat in the map so pick the last one (?)
-              (-> result
-                  (update :stay + (get doors choice))
-                  (update :switch + switch))))
-          {:stay 0 :switch 0} (range n)))
+  (let [choices (take n (repeatedly #(rand-int 3)))]
+    (reduce (fn [result choice]
+              (let [switch (val (last (dissoc doors choice)))]
+                ;; out of the 2 remaining doors, the first will be a goat in the map so pick the last one (?)
+                (-> result
+                    (update :stay + (get doors choice))
+                    (update :switch + switch))))
+            {:stay 0 :switch 0} choices)))
